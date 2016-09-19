0dbf04fc67c4429ae1cd247ec978993611dfe288
praneethkumarpidugu <pidugu2007@gmail.com>
2016-09-18 20:08:01 -0400

    Backend signup

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,49 @@
  */
 
 module.exports = {
-	
+	signup: function (req, res) {
+    console.log('Backend signup');
+    var Passwords = require('machinepack-passwords');
+
+    //Encrypt Passwords
+    Passwords.encryptPassword({
+      password: req.param('password'),
+      difficulty: 10
+    }).exec({
+      error: function (err) {
+        return res.negotiate(err);
+      },
+      success: function (encryptedPassword) {
+        require('machinepack-gravatar').getImageUrl({
+          emailAddress: req.param('email')
+        }).exec({
+          error: function (err) {
+            return res.negotiate(err);
+          },
+          success: function (gravatarUrl){
+            User.create({
+              name: req.param('name'),
+              email: req.param('email'),
+              password: encryptedPassword,
+              lastLoggedIn: new Date(),
+              gravatarUrl: gravatarUrl
+            }, function userCreated(err, newUser) {
+              if (err) {
+                console.log('Error: '+ err);
+                return res.negotiate(err);
+              }
+
+              //SESSION VAR
+              console.log('User Created');
+
+              res.json({
+                id: newUser.id
+              });
+            })
+          }
+        })
+      }
+    })
+  }
 };
 
--- assets/js/public/signup/SignupCtrl.js
+++ assets/js/public/signup/SignupCtrl.js
@@ -9,15 +9,15 @@ angular
     $scope.runSignup = function () {
       console.log('Signing up ' + $scope.name);
       $http.post('/signup', {
-        name: $scope.name,
-        email:$scope.email,
-        password:$scope.passwords
+        name:  $scope.name,
+        email: $scope.email,
+        password: $scope.password
       })
         .then(function onSuccess(response) {
           window.location = '/user'
         })
         .catch(function onError(err) {
-          console.log('Error ' + err);
+          console.log('Error ', err);
         })
     }
   }]);
